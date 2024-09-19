01df3952c72642fa0edb7dd5bfb51768328dd948
Sean <sberndlmaier@proton.me>
2024-09-19 12:50:29 -0400

    Finished Step 2 - CSS styling using directly imported stylesheets

--- styling-tutorial/src/components/App/App.js
+++ styling-tutorial/src/components/App/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
 import './App.css';
 
+import Alert from '../Alert/Alert';
+import CartSuccess from '../CartSuccess/CartSuccess';
+
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <Alert title="Items Not Added" type="error">
+        <div>Your items are out of stock.</div>
+      </Alert>
+      <CartSuccess />
+    </div>
+  )
 }
 
 export default App;
