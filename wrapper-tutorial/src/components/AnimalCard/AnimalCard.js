cb2fb89e38eb78c01fe30cf6eed9e6c75cd30971
Sean Berndlmaier <138079299+seanb1526@users.noreply.github.com>
2024-09-18 20:28:56 -0400

    Stopped on Step 3

---
+++ wrapper-tutorial/src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AnimalDetails from '../AnimalDetails/AnimalDetails';
+
+export default function AnimalCard({ name, size , ...props}) {
+    return (
+        <div>
+            <h3>{name}</h3>
+            <div>{size}kg</div>
+            <AnimalDetails
+                {...props}
+            />
+        </div>
+    )
+}
+
+AnimalCard.propTypes = {
+    // diet: PropTypes.arrayOf(PropTypes.string).isRequired,
+    name: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired,
+}
