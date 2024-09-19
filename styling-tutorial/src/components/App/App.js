cbd46cdd38745b5263390874bb4f50754c604522
Sean <sberndlmaier@proton.me>
2024-09-19 14:07:45 -0400

    Finished

--- styling-tutorial/src/components/App/App.js
+++ styling-tutorial/src/components/App/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
 
 import Alert from '../Alert/Alert';
 import CartSuccess from '../CartSuccess/CartSuccess';
 
 function App() {
-  return (
-    <div className="wrapper">
+  const wrapper = {
+    padding: 20
+  };
+
+  return(
+    <div style={wrapper}>
       <Alert title="Items Not Added" type="error">
         <div>Your items are out of stock.</div>
       </Alert>
