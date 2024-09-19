16d951c51962fdc3ec9f9f841584a3d04261d35a
Sean <sberndlmaier@proton.me>
2024-09-19 14:23:25 -0400

    Step 1 complete

---
+++ state-hooks/src/components/Product/Product.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Product.css';
+
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+}
+
+function getTotal(total) {
+  return total.toLocaleString(undefined, currencyOptions)
+}
+
+export default function Product() {
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+
+  return(
+    <div className="wrapper">
+      <div>
+        Shopping Cart: {cart.length} total items.
+      </div>
+      <div>Total: {getTotal(total)}</div>
+
+      <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+      <button>Add</button> <button>Remove</button>
+    </div>
+  )
+}
